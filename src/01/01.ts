958bd9351b37fe93ffe43a4c0879dd2b31cd41ce
Erik Mejer Hansen <erik.mejer.hansen@gmail.com>
2021-12-01 15:56:32 +0100

    ✨ Solved 01 part 2

--- src/01/01.ts
+++ src/01/01.ts
@@ -18,5 +18,20 @@ export const countNumberOfIncreases = (measurements: number[]): number => {
   }).length
 }
 
+export const countNumberOfIncreasingWindows = (measurements: number[]): number => {
+  return measurements.filter((value, index) => {
+    // Don't start counting until we have enough to make the window
+    if (index < 2) {
+      return false
+    }
+
+    const a = measurements[index - 2] + measurements[index - 1] + measurements[index]
+    const b = measurements[index - 1] + measurements[index] + measurements[index + 1]
+
+    return a < b
+  }).length
+}
+
 const measurements = parseData('./src/01/data/partOne.txt')
 console.log('Number on increases:', countNumberOfIncreases(measurements))
+console.log('Number on increasing windows:', countNumberOfIncreasingWindows(measurements))
--- src/01/__tests__/01.test.ts
+++ src/01/__tests__/01.test.ts
@@ -1,13 +1,19 @@
-import { countNumberOfIncreases } from '../01_01'
+import { countNumberOfIncreases, countNumberOfIncreasingWindows } from '../01'
 
 describe('Dec 1', () => {
+  const sampleData = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
   describe('Part 1', () => {
-    const sampleData = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     it('should not mark the first index as increasing', () => {
       expect(countNumberOfIncreases([0, 10])).toBe(1)
     })
     it('should find 7 measurements that are increasing', () => {
       expect(countNumberOfIncreases(sampleData)).toBe(7)
+    })
+  })
+
+  describe('Part 2', () => {
+    it('should find 5 windows that are increasing', () => {
+      expect(countNumberOfIncreasingWindows(sampleData)).toBe(5)
     })
   })
 })
