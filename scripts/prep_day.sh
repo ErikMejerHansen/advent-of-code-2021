624c4659856133b80e8b2db03a1ac2bbd7b3d943
Erik Mejer Hansen <erik.mejer.hansen@gmail.com>
2021-12-08 07:26:35 +0100

    ✨ Make starting a new day easier

---
+++ scripts/prep_day.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Creating files for" $1
+
+mkdir src/$1
+mkdir src/$1/__tests__
+mkdir src/$1/data
+
+touch src/$1/$1.ts  
+echo "import * as fs from 'fs'
+
+const data = fs.readFileSync('./src/${1}/data/data.txt').toString()
+" > src/$1/$1.ts
+
+
+touch src/$1/__tests__/$1.test.ts  
+echo "describe('Dec ${1}', ()=> {
+    describe('Part 1', ()=> {
+        //
+    })
+    
+    describe('Part 2', ()=>{
+        //
+    })
+})" > src/$1/__tests__/$1.test.ts
