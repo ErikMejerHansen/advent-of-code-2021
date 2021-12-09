2d91ba4058320f2d90483edc395b287a1942c618
Erik Mejer Hansen <erik.mejer.hansen@gmail.com>
2021-12-09 10:30:53 +0100

    ✨  Solved Dec 9, part 2

--- src/09/09.ts
+++ src/09/09.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs'
-import { calculateGameScore } from 'src/04/04'
+
+type Coordinate = [number, number]
 
 export const parseHeightMap = (data: string): number[][] => {
   return data.split('\n').map(line => line.split('').map(height => parseInt(height, 10)))
 }
 
-export const findLowPoints = (heightMap: number[][]): number[] => {
-  const lowPoints = new Array<number>()
+export const findLowPoints = (heightMap: number[][]): Coordinate[] => {
+  const lowPoints = new Array<Array<number>>()
   for (let rowIndex = 0; rowIndex < heightMap.length; rowIndex++) {
     const row = heightMap[rowIndex]
     for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
@@ -23,18 +24,88 @@ export const findLowPoints = (heightMap: number[][]): number[] => {
         columnIndex < row.length - 1 ? height < heightMap[rowIndex][columnIndex + 1] : true
 
       if (smallerThanNeighborUp && smallerThanNeighborDown && smallerThanNeighborLeft && smallerThanNeighborRight) {
-        lowPoints.push(height)
+        lowPoints.push([rowIndex, columnIndex])
       }
     }
   }
-  return lowPoints
+
+  return lowPoints as Coordinate[]
 }
 
-export const calculateRiskScore = (lowPoints: number[]): number =>
-  lowPoints.reduce((riskScore, height) => riskScore + 1 + height, 0)
+const getNeighborsCoordinates = ([x, y]: Coordinate, array: number[][]): Coordinate[] => {
+  const neighbors: [number, number][] = new Array<[number, number]>()
+
+  // add upwards neighbor
+  if (x > 0) {
+    neighbors.push([x - 1, y])
+  }
+
+  // add downwards neighbor
+  if (x < array.length - 1) {
+    neighbors.push([x + 1, y])
+  }
+
+  // add leftwards neighbor
+  if (y > 0) {
+    neighbors.push([x, y - 1])
+  }
+  // add leftwards neighbor
+  if (y < array[x].length - 1) {
+    neighbors.push([x, y + 1])
+  }
+
+  return neighbors
+}
+
+const filterCoordinatesByValue = (coordinates: Coordinate[], array: number[][], exclude: number): Coordinate[] =>
+  coordinates.filter(([x, y]) => array[x][y] !== exclude)
+
+export const calculateRiskScore = (lowPoints: Coordinate[], heightMap: number[][]): number =>
+  lowPoints.reduce((riskScore, lowPoint) => riskScore + 1 + heightMap[lowPoint[0]][lowPoint[1]], 0)
+
+const findBasinSize = (coordinate: Coordinate, heightMap: number[][]): number => {
+  const basin = new Set<string>()
+  // Find the first round of neighbors
+  const neighbors = getNeighborsCoordinates(coordinate, heightMap)
+  // Filter out any neighbor with a height of 9
+  let nextNeighbors = filterCoordinatesByValue(neighbors, heightMap, 9)
+
+  // Now that we have our first rounds of candidates let's keep going until we can't find anymore
+  while (nextNeighbors.length !== 0) {
+    // First save the current neighbors. We convert the coordinates to strings to make equality checks simple
+    nextNeighbors.forEach(coordinate => basin.add(`${coordinate[0]},${coordinate[1]}`))
+
+    // Get the neighbors of the cells we're currently looking at
+    const nextCandidates = nextNeighbors.flatMap(coordinate => getNeighborsCoordinates(coordinate, heightMap))
+
+    // Filter out the ones we already know
+    const newCandidates = nextCandidates.filter(coordinate => !basin.has(`${coordinate[0]},${coordinate[1]}`))
+
+    // Once we filter out the ones with hight 9 we have our next rounds of candidates
+    nextNeighbors = filterCoordinatesByValue(newCandidates, heightMap, 9)
+  }
+
+  // The basin size is then just the size of the Set of unique coordinates
+  return basin.size
+}
+export const findBasinSizes = (lowPoints: Coordinate[], heightMap: number[][]): number[] =>
+  lowPoints.map(coordinate => findBasinSize(coordinate, heightMap))
+
+export const calculateBasinProduct = (basinSizes: number[]): number => {
+  const result = basinSizes
+    .sort((a, b) => a - b)
+    .reverse()
+    .splice(0, 3)
+    .reduce((product, size) => product * size, 1)
+
+  return result
+}
 
 const data = fs.readFileSync('./src/09/data/data.txt').toString()
 const heightMap = parseHeightMap(data)
 const lowPoints = findLowPoints(heightMap)
 
-console.log('Risk score:', calculateRiskScore(lowPoints))
+const basinSizes = findBasinSizes(lowPoints, heightMap)
+
+console.log('Risk score:', calculateRiskScore(lowPoints, heightMap))
+console.log('Basin product:', calculateBasinProduct(basinSizes))
--- src/09/__tests__/09.test.ts
+++ src/09/__tests__/09.test.ts
@@ -1,4 +1,4 @@
-import { calculateRiskScore, findLowPoints, parseHeightMap } from '../09'
+import { calculateBasinProduct, calculateRiskScore, findBasinSizes, findLowPoints, parseHeightMap } from '../09'
 
 describe('Dec 09', () => {
   const sampleData = '2199943210\n' + '3987894921\n' + '9856789892\n' + '8767896789\n' + '9899965678'
@@ -9,18 +9,36 @@ describe('Dec 09', () => {
 
     it('finds the low-points in the sample data', () => {
       const heightMap = parseHeightMap(sampleData)
-      expect(findLowPoints(heightMap)).toStrictEqual([1, 0, 5, 5])
+      expect(findLowPoints(heightMap)).toStrictEqual([
+        [0, 1],
+        [0, 9],
+        [2, 2],
+        [4, 6],
+      ])
     })
 
     it('calculates the correct risk level on the sample data', () => {
       const heightMap = parseHeightMap(sampleData)
       const lowPoints = findLowPoints(heightMap)
 
-      expect(calculateRiskScore(lowPoints)).toBe(15)
+      expect(calculateRiskScore(lowPoints, heightMap)).toBe(15)
     })
   })
 
   describe('Part 2', () => {
-    //
+    it('finds the 4 basin scores for the sample data', () => {
+      const heightMap = parseHeightMap(sampleData)
+      const lowPoints = findLowPoints(heightMap)
+
+      expect(findBasinSizes(lowPoints, heightMap)).toStrictEqual([3, 9, 14, 9])
+    })
+
+    it('calculates the correct basin product from the sample data', () => {
+      const heightMap = parseHeightMap(sampleData)
+      const lowPoints = findLowPoints(heightMap)
+      const basisSizes = findBasinSizes(lowPoints, heightMap)
+
+      expect(calculateBasinProduct(basisSizes)).toBe(1134)
+    })
   })
 })
