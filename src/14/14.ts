e07834de41c2ca617e9b32138cac23f1692cdf4b
Erik Mejer Hansen <erik.mejer.hansen@gmail.com>
2021-12-15 19:47:19 +0100

    ✨ Solved Dec 14, part 2

--- src/14/14.ts
+++ src/14/14.ts
@@ -1,48 +1,66 @@
 import * as fs from 'fs'
 
+export type PairCounts = Map<string, number>
 interface PolymerizationInstructions {
-  template: string
-  insertions: Map<string, string>
+  template: PairCounts
+  insertions: Map<string, [string, string]>
+}
+
+export const stringToPairCount = (input: string): PairCounts => {
+  const inputSplit = input.split('')
+  const counts = inputSplit.reduce((map, character, index) => {
+    if (index === inputSplit.length - 1) {
+      return map
+    }
+    const pair = character + inputSplit[index + 1]
+    upsert(map, pair, (value = 0) => value + 1)
+    return map
+  }, new Map())
+
+  return counts
 }
 
 export const parsePolymerizationInstructions = (data: string): PolymerizationInstructions => {
   const split = data.split('\n')
-  const template = split.slice()[0]
+  const templatePairCounts = stringToPairCount(split.slice()[0])
 
   // Remove the empty line between the template and the pair insertions section
   const insertions = split.slice(2).reduce((map, line) => {
     const [pair, insertion] = line.split(' -> ')
-    map.set(pair, insertion)
+    const [left, right] = pair.split('')
+    map.set(pair, [left + insertion, insertion + right])
     return map
-  }, new Map<string, string>())
+  }, new Map<string, [string, string]>())
 
-  return { template, insertions }
+  return { template: templatePairCounts, insertions }
 }
 
-export const polymerize = (polymer: string, insertions: Map<string, string>): string => {
-  // Split the polymer into individual characters
-  const splitPolymer = polymer.split('')
-  // Then start building the new polymer
-  return splitPolymer.reduce((polymer, currentCharacter, index) => {
-    // We form pairs by taking the current character and the next one in the array. So we need to bail before reaching the end of the array
-    if (index === splitPolymer.length - 1) {
-      return polymer + currentCharacter
-    }
-    const nextCharacter = splitPolymer[index + 1]
+const upsert = <K, V>(map: Map<K, V>, key: K, updater: (previous?: V) => V, verbose = false) => {
+  if (verbose) {
+    console.log('Upsert:', key, 'into:', map)
+  }
+  const newValue = updater(map.get(key))
+  map.set(key, newValue)
+}
 
-    // Look up potential insertions
-    const insertion = insertions.get(currentCharacter + nextCharacter)
-    if (insertion) {
-      // If a insertion is found insert it into the polymer we're building. The trailing end of the pair gets added next time trough here.
-      return polymer + currentCharacter + insertion
+export const polymerize = (polymer: PairCounts, insertions: Map<string, [string, string]>): PairCounts => {
+  const entries = [...polymer.entries()]
+
+  const nextPolymer = new Map()
+  entries.forEach(([pair, count]) => {
+    if (insertions.has(pair)) {
+      const [left, right] = insertions.get(pair)!
+      upsert(nextPolymer, left, (value = 0) => value + count, false)
+      upsert(nextPolymer, right, (value = 0) => value + count, false)
     } else {
-      // ... otherwise just return the polymer as is
-      return polymer + currentCharacter
+      upsert(nextPolymer, pair, (value = 0) => value + count)
     }
-  }, '')
+  })
+
+  return nextPolymer
 }
 
-export const runPolymerization = ({ template, insertions }: PolymerizationInstructions, steps: number): string => {
+export const runPolymerization = ({ template, insertions }: PolymerizationInstructions, steps: number): PairCounts => {
   let polymer = template
   for (let i = 0; i < steps; i++) {
     polymer = polymerize(polymer, insertions)
@@ -50,28 +68,36 @@ export const runPolymerization = ({ template, insertions }: PolymerizationInstru
   return polymer
 }
 
-const frequencyCount = (data: string): Map<string, number> =>
-  data.split('').reduce((map, char) => {
-    const currentCount = map.get(char)
-    if (currentCount) {
-      map.set(char, currentCount + 1)
-    } else {
-      map.set(char, 1)
-    }
-    return map
-  }, new Map<string, number>())
+export const frequencyCount = (pairCounts: PairCounts): number[] => {
+  let frequencies = Array(26).fill(0)
+  const pairEntries = [...pairCounts.entries()]
 
-export const part1 = (polymer: string): number => {
+  console.log(pairEntries)
+  for (let i = 0; i < pairEntries.length; i++) {
+    const [pair, count] = pairEntries[i]
+    const [left, right] = pair.split('')
+
+    const indexOfLeft = left.codePointAt(0)! - 65
+    frequencies[indexOfLeft] += count
+
+    const indexOfRight = right.codePointAt(0)! - 65
+    frequencies[indexOfRight] += count
+  }
+
+  return frequencies.map(it => Math.ceil(it / 2))
+}
+
+export const mostMinusLeast = (polymer: PairCounts): number => {
   const frequencies = frequencyCount(polymer)
-  const inOrderOfFrequency = [...frequencies.entries()].sort(([_charA, a], [_charB, b]) => b - a)
-  const mostFrequent = inOrderOfFrequency[0][1]
-  const leastFrequent = inOrderOfFrequency[inOrderOfFrequency.length - 1][1]
+  const inOrderOfFrequency = frequencies.sort((a, b) => b - a).filter(count => count > 0)
+  const mostFrequent = inOrderOfFrequency[0]
+  const leastFrequent = inOrderOfFrequency[inOrderOfFrequency.length - 1]
 
   return mostFrequent - leastFrequent
 }
 
 const data = fs.readFileSync('./src/14/data/data.txt').toString()
 const instructions = parsePolymerizationInstructions(data)
-const polymer = runPolymerization(instructions, 10)
 
-console.log('Part 1:', part1(polymer))
+console.log('Most minus least after 10 rounds', mostMinusLeast(runPolymerization(instructions, 10)))
+console.log('Most minus least after 40 rounds', mostMinusLeast(runPolymerization(instructions, 40)))
--- src/14/__tests__/14.test.ts
+++ src/14/__tests__/14.test.ts
@@ -1,6 +1,14 @@
 import * as fs from 'fs'
 
-import { parsePolymerizationInstructions, part1, polymerize, runPolymerization } from '../14'
+import {
+  frequencyCount,
+  PairCounts,
+  parsePolymerizationInstructions,
+  mostMinusLeast,
+  polymerize,
+  runPolymerization,
+  stringToPairCount,
+} from '../14'
 
 describe('Dec 14', () => {
   describe('Part 1', () => {
@@ -25,47 +33,109 @@ describe('Dec 14', () => {
       'CN -> C'
 
     it('can parse the sample input', () => {
-      expect(parsePolymerizationInstructions(sampleData).template).toBe('NNCB')
-      expect(parsePolymerizationInstructions(sampleData).insertions.get('BB')).toBe('N')
-      expect(parsePolymerizationInstructions(sampleData).insertions.get('CN')).toBe('C')
-      expect(parsePolymerizationInstructions(sampleData).insertions.get('CH')).toBe('B')
+      expect(parsePolymerizationInstructions(sampleData).template).toStrictEqual(stringToPairCount('NNCB'))
+
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('CH')).toStrictEqual(['CB', 'BH'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('HH')).toStrictEqual(['HN', 'NH'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('CB')).toStrictEqual(['CH', 'HB'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('NH')).toStrictEqual(['NC', 'CH'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('HB')).toStrictEqual(['HC', 'CB'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('HN')).toStrictEqual(['HC', 'CN'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('NN')).toStrictEqual(['NC', 'CN'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('BH')).toStrictEqual(['BH', 'HH'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('NC')).toStrictEqual(['NB', 'BC'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('NB')).toStrictEqual(['NB', 'BB'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('BN')).toStrictEqual(['BB', 'BN'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('BB')).toStrictEqual(['BN', 'NB'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('BC')).toStrictEqual(['BB', 'BC'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('CC')).toStrictEqual(['CN', 'NC'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('BB')).toStrictEqual(['BN', 'NB'])
+      expect(parsePolymerizationInstructions(sampleData).insertions.get('CN')).toStrictEqual(['CC', 'CN'])
     })
 
     it.each([
-      ['CH', 'CBH'],
-      ['HH', 'HNH'],
-      ['CB', 'CHB'],
-      ['NH', 'NCH'],
-      ['HB', 'HCB'],
-      ['HC', 'HBC'],
-      ['HN', 'HCN'],
-      ['NN', 'NCN'],
-      ['BH', 'BHH'],
-      ['NC', 'NBC'],
-      ['NB', 'NBB'],
-      ['BN', 'BBN'],
-      ['BB', 'BNB'],
-      ['BC', 'BBC'],
-      ['CC', 'CNC'],
-      ['CN', 'CCN'],
+      ['CH', ['CB', 'BH']],
+      ['HH', ['HN', 'NH']],
+      ['CB', ['CH', 'HB']],
+      ['NH', ['NC', 'CH']],
+      ['HB', ['HC', 'CB']],
+      ['HC', ['HB', 'BC']],
+      ['HN', ['HC', 'CN']],
+      ['NN', ['NC', 'CN']],
+      ['NC', ['NB', 'BC']],
+      ['BB', ['BN', 'NB']],
+      ['CC', ['CN', 'NC']],
     ])('can compute the insertions', (input, expectedPolymer) => {
       const instructions = parsePolymerizationInstructions(sampleData)
-      expect(polymerize(input, instructions.insertions)).toBe(expectedPolymer)
+      const expectedPairCount = new Map(expectedPolymer.map(pair => [pair, 1]))
+      expect(polymerize(new Map([[input, 1]]), instructions.insertions)).toStrictEqual(expectedPairCount)
     })
 
     it('leaves the polymer intact if there are no matching insertions', () => {
       const instructions = parsePolymerizationInstructions(sampleData)
-      expect(polymerize('ZZ', instructions.insertions)).toBe('ZZ')
+      const polymer = stringToPairCount('ZZ')
+      expect(polymerize(polymer, instructions.insertions)).toStrictEqual(polymer)
     })
 
     it.each([
-      [1, 'NCNBCHB'],
-      [2, 'NBCCNBBBCBHCB'],
-      [3, 'NBBBCNCCNBBNBNBBCHBHHBCHB'],
-      [4, 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'],
-    ])('after %d steps it builds %s', (steps, expectedPolymer) => {
+      [1, stringToPairCount('NCNBCHB')],
+      [2, stringToPairCount('NBCCNBBBCBHCB')],
+      [3, stringToPairCount('NBBBCNCCNBBNBNBBCHBHHBCHB')],
+      [4, stringToPairCount('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')],
+    ])('after %d steps it builds %s', (steps: number, expectedPolymer: PairCounts) => {
       const instructions = parsePolymerizationInstructions(sampleData)
-      expect(runPolymerization(instructions, steps)).toBe(expectedPolymer)
+      const polymer = runPolymerization(instructions, steps)
+      const expectedEntries = [...expectedPolymer.entries()]
+      expectedEntries.forEach(([pair, count]) => {
+        expect(polymer.get(pair)).toBe(count)
+      })
+    })
+
+    it('can count letter frequencies correctly', () => {
+      let expectedFrequencies = Array(26).fill(0)
+      expectedFrequencies['B'.charCodeAt(0) - 65] = 1
+      expectedFrequencies['C'.charCodeAt(0) - 65] = 1
+      expectedFrequencies['N'.charCodeAt(0) - 65] = 2
+      expect(frequencyCount(stringToPairCount('NNCB'))).toStrictEqual(expectedFrequencies)
+
+      expectedFrequencies.fill(0)
+      expectedFrequencies['B'.charCodeAt(0) - 65] = 2
+      expectedFrequencies['C'.charCodeAt(0) - 65] = 2
+      expectedFrequencies['H'.charCodeAt(0) - 65] = 1
+      expectedFrequencies['N'.charCodeAt(0) - 65] = 2
+      expect(frequencyCount(stringToPairCount('NCNBCHB'))).toStrictEqual(expectedFrequencies)
+
+      expectedFrequencies.fill(0)
+      expectedFrequencies['B'.charCodeAt(0) - 65] = 6
+      expectedFrequencies['C'.charCodeAt(0) - 65] = 4
+      expectedFrequencies['H'.charCodeAt(0) - 65] = 1
+      expectedFrequencies['N'.charCodeAt(0) - 65] = 2
+      expect(frequencyCount(stringToPairCount('NBCCNBBBCBHCB'))).toStrictEqual(expectedFrequencies)
+
+      expectedFrequencies.fill(0)
+      expectedFrequencies['B'.charCodeAt(0) - 65] = 11
+      expectedFrequencies['C'.charCodeAt(0) - 65] = 5
+      expectedFrequencies['H'.charCodeAt(0) - 65] = 4
+      expectedFrequencies['N'.charCodeAt(0) - 65] = 5
+      expect(frequencyCount(stringToPairCount('NBBBCNCCNBBNBNBBCHBHHBCHB'))).toStrictEqual(expectedFrequencies)
+
+      expectedFrequencies.fill(0)
+      expectedFrequencies['B'.charCodeAt(0) - 65] = 23
+      expectedFrequencies['C'.charCodeAt(0) - 65] = 10
+      expectedFrequencies['H'.charCodeAt(0) - 65] = 5
+      expectedFrequencies['N'.charCodeAt(0) - 65] = 11
+      expect(frequencyCount(stringToPairCount('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'))).toStrictEqual(
+        expectedFrequencies
+      )
+    })
+
+    it('calculates the correct part 1 product on the sample data after 10 steps', () => {
+      const sample = parsePolymerizationInstructions(sampleData)
+      const polymer = runPolymerization(sample, 10)
+      const freqs = frequencyCount(polymer)
+      console.log(freqs)
+      const result = mostMinusLeast(polymer)
+      expect(result).toBe(1588)
     })
 
     it('Part 1 solution is 2967', () => {
@@ -73,10 +143,16 @@ describe('Dec 14', () => {
       const instructions = parsePolymerizationInstructions(data)
       const polymer = runPolymerization(instructions, 10)
 
-      expect(part1(polymer)).toBe(2967)
+      expect(mostMinusLeast(polymer)).toBe(2967)
     })
   })
   describe('Part 2', () => {
-    //
+    it('Part 2 solution is 3692219987038', () => {
+      const data = fs.readFileSync('./src/14/data/data.txt').toString()
+      const instructions = parsePolymerizationInstructions(data)
+      const polymer = runPolymerization(instructions, 40)
+
+      expect(mostMinusLeast(polymer)).toBe(3692219987038)
+    })
   })
 })
